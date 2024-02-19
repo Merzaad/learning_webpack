5caecc80b264e6c21f9942211445dd2fcace8708
mehrzad <merzaad@gmail.com>
2024-02-19 12:08:38 +0330

    hash name, source map, dev server, bable loader, image loader, bundle analyzer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import print from "./module";
 import "../src/styles/main.scss";
+import webpackLogo from "./assets/webpack.png";
 
-print("Hello World!");
+print("Hello World!!");
+document
+  .getElementById("webpackLogo")
+  .appendChild(document.createElement("img")).src = webpackLogo;
